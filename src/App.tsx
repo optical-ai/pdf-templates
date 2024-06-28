42f9d07b0eaaa9d4a0480214e45328e22b004cd9
Arun Singh <arunsingh28aug.as@gmail.com>
2024-06-28 15:54:39 +0530

    feat: Add lazy loading for images and update landing page banner color

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import Document from './Document'
 
-import pdf from './assets/ArunSinghCV.pdf'
+import { variables } from './constant'
+// import pdf from './assets/Payslip.pdf'
 
 const App = () => {
   return (
     <div>
-      <Document coordinates={[
-         { x: 100, y: 150, width: 200, height: 50 },
-         { x: 300, y: 400, width: 150, height: 100 },
-      ]} pdf={pdf}/>
+      <Document pdf={variables.base64}/>
     </div>
   )
 }
 
 export default App
+
--- src/Document.tsx
+++ src/Document.tsx
@@ -1,86 +1,30 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { Document, Page, pdfjs } from 'react-pdf';
-import { PDFDocumentProxy } from 'pdfjs-dist';
-
-// Set the worker path to the provided static asset
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    'pdfjs-dist/build/pdf.worker.min.mjs',
-    import.meta.url,
-  ).toString();
-
-interface Coordinates {
-  x: number;
-  y: number;
-  width: number;
-  height: number;
-}
-
-interface MyDocumentProps {
-  pdf: string;
-  coordinates: Coordinates[];
-}
-
-const MyDocument: React.FC<MyDocumentProps> = ({ pdf, coordinates }) => {
-  const [numPages, setNumPages] = useState<number | null>(null);
-  const canvasRefs = useRef<(HTMLCanvasElement | null)[]>([]);
-
-  useEffect(() => {
-    canvasRefs.current = canvasRefs.current.slice(0, numPages || 0);
-  }, [numPages]);
-
-  const onDocumentLoadSuccess = ({ numPages }: PDFDocumentProxy) => {
-    setNumPages(numPages);
-  };
-
-  const drawRectangles = (canvas: HTMLCanvasElement, coords: Coordinates[]) => {
-    const context = canvas.getContext('2d');
-    if (context) {
-      context.strokeStyle = 'red';
-      context.lineWidth = 2;
-      coords.forEach(coord => {
-        context.strokeRect(coord.x, coord.y, coord.width, coord.height);
-      });
-    }
-  };
-
-  const onRenderSuccess = (pageNumber: number) => {
-    const canvas = canvasRefs.current[pageNumber - 1];
-    if (canvas) {
-      const context = canvas.getContext('2d');
-      if (context) {
-        const transform = context.getTransform();
-        const scaledCoords = coordinates.map(coord => ({
-          x: coord.x * transform.a + transform.e,
-          y: coord.y * transform.d + transform.f,
-          width: coord.width * transform.a,
-          height: coord.height * transform.d,
-        }));
-        drawRectangles(canvas, scaledCoords);
-      }
-    }
-  };
-
+const Viewer = ({ pdf }: { pdf: string }) => {
   return (
-    <Document
-      file={pdf}
-      onLoadSuccess={onDocumentLoadSuccess}
+    <svg
+      x="100"
+      y="100"
+      width="1240"
+      height="1605"
+      viewBox="0 0 1240 1605"
+      preserveAspectRatio="meet"
+      scale={0.1}
+      cursor={"crosshair"}
     >
-      {Array.from(new Array(numPages || 0), (el, index) => (
-        <Page
-          key={`page_${index + 1}`}
-          pageNumber={index + 1}
-          renderAnnotationLayer={false}
-          renderTextLayer={false}
-          onRenderSuccess={() => onRenderSuccess(index + 1)}
-          canvasRef={ref => {
-            canvasRefs.current[index] = ref;
-          }}
-        />
-      ))}
-    </Document>
+      <image href={pdf} width="1240" height="1605" />
+      <rect
+        x="200"
+        y="166"
+        width="256"
+        height="41"
+        vector-effect="non-scaling-stroke"
+        pointer-events="all"
+        stroke-width="2"
+        rx="6.381093057607091"
+        ry="6.381093057607091"
+        className="css-ef7n63"
+      ></rect>
+    </svg>
   );
 };
 
-export default MyDocument;
-
-
+export default Viewer;
